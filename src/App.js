d22b45de3fbbdf889fa218d52e73c07195d7c423
noormohammad20 <mdsaim204@gmail.com>
2022-04-05 22:04:19 +0600

    add firebase config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css'
-
+import app from './firebase.init'
+import { getAuth } from "firebase/auth"
+const auth = getAuth(app)
 function App() {
   return (
     <div className="App">
--- src/_steps.js
+++ src/_steps.js
@@ -3,5 +3,10 @@
 1. create a project on console.firebase.google.com
 2. install firebase 
 3. register web app in firebase 
-4. 
+4. copy firebase init with config from firebase project settings into a file firebase.init.js 
+5. export default app .
+6. import app firebase init.js into your app.js
+7. import getAuth from firebase/auth and create auth = getAuth(app)
+8. turn of google authentication
+
 */
