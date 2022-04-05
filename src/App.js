aa07293f30f4095e2eaed7e11fd985b5f94119dd
noormohammad20 <mdsaim204@gmail.com>
2022-04-06 00:13:31 +0600

    Sign in with google, open google login popup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css'
 import app from './firebase.init'
-import { getAuth } from "firebase/auth"
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
 const auth = getAuth(app)
+
 function App() {
+  const provider = new GoogleAuthProvider()
+  const handleGoogleSingIn = () => {
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const user = result.user
+        console.log(user)
+      })
+      .catch(error => {
+        console.log('error', error)
+      })
+  }
   return (
     <div className="App">
-
+      <button onClick={handleGoogleSingIn}>Google Sign-In</button>
     </div>
   )
 }
