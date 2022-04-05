6f8f47779c7a97ec9fbabf5496acea75c5a17c35
noormohammad20 <mdsaim204@gmail.com>
2022-04-05 21:39:04 +0600

    install firebase and add firebase configuration

---
+++ src/_steps.js
@@ -0,0 +1,7 @@
+/* 
+* steps to use firebase 
+1. create a project on console.firebase.google.com
+2. install firebase 
+3. register web app in firebase 
+4. 
+*/
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB05HFVQvbqw8C9ikN8JmXKQ7-uCADrH0o",
+    authDomain: "simple-firebase-authenti-57a51.firebaseapp.com",
+    projectId: "simple-firebase-authenti-57a51",
+    storageBucket: "simple-firebase-authenti-57a51.appspot.com",
+    messagingSenderId: "437189760973",
+    appId: "1:437189760973:web:816f9f6c2688eabdad5684"
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+export default app
